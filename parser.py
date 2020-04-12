1e690e5e6ba8768c983039837631a21b1a5b2ecb
Dreamer <dreamerandprogrammer@gmail.com>
2020-04-12 10:51:57 +0300

    Add username as variable

--- parser.py
+++ parser.py
@@ -1,11 +1,13 @@
+# GitHub Contributions parser by DreamerDeLy
+
 from bs4 import BeautifulSoup
 import requests as req
 
-resp = req.get("https://github.com/DreamerDeLy?tab=overview&from=2020-01-01&to=2020-12-01")
- 
+username = "DreamerDeLy"
+
+resp = req.get("https://github.com/" + username + "?tab=overview&from=2020-01-01&to=2020-12-01")
 soup = BeautifulSoup(resp.text, 'lxml')
- 
-#print(soup.title)
+
 print("read page: \"" + soup.title.text + "\"")
 
 text = soup.find('h2', 'f4 text-normal mb-2').text.strip()
@@ -25,7 +27,7 @@ day_number = day_number_1.days
 commits_per_day = commits / day_number
 commits_year_forecast = commits_per_day * 365
 
-print("\nAnalitics")
+print("\nAnalitics:")
 print("Commits per day: \t" + str(round(commits_per_day, 2)))
 print("Commits forecast: \t" + str(round(commits_year_forecast)))
 
