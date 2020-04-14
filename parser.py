a4345cd98d8f6a16ed34dd571bafef43a14c4793
DreamerDeLy <dreamerandprogrammer@gmail.com>
2020-04-14 12:39:53 +0300

    Add finding max commits per day

--- parser.py
+++ parser.py
@@ -11,12 +11,9 @@ soup = BeautifulSoup(resp.text, 'lxml')
 print("read page: \"" + soup.title.text + "\"")
 
 text = soup.find('h2', 'f4 text-normal mb-2').text.strip()
-
-print("text of block: \"" + text + "\"")
-
 commits = int(text.split()[0])
 
-print("number: \"" + str(commits) + "\"")
+# ------------------------------------------------------------------------------
 
 from datetime import date
 d0 = date(2020, 1, 1)
@@ -27,10 +24,7 @@ day_number = day_number_1.days
 commits_per_day = commits / day_number
 commits_year_forecast = commits_per_day * 365
 
-print("\nAnalitics:")
-print("Commits per day: \t" + str(round(commits_per_day, 2)))
-print("Commits forecast: \t" + str(round(commits_year_forecast)))
-print("\n")
+# ------------------------------------------------------------------------------ 
 
 text_calendar = "" 
 add_text = "0"
@@ -43,5 +37,28 @@ for tag in soup.find_all("rect", "day"):
 	dates.append(tag["data-date"])
 	counts.append(tag["data-count"])
 
+#for i in range(len(dates)):
+#	print("Date: {0}, Count: {1}".format(dates[i], counts[i]))
+
+# ------------------------------------------------------------------------------
+
+commits_max = 0
+commits_max_day = ""
+
 for i in range(len(dates)):
-	print("Date: {0}, Count: {1}".format(dates[i], counts[i]))
+	commits_ = int(counts[i]) 
+
+	if (commits_ > commits_max):
+		commits_max = commits_
+		commits_max_day = dates[i]
+
+# ------------------------------------------------------------------------------
+
+print("\nAnalitics:")
+print("Commits per year: \t"+str(commits))
+print("Max commits: {0} ({1})".format(commits_max, commits_max_day))
+print("\n")
+print("Commits per day: \t" + str(round(commits_per_day, 2)))
+print("Commits forecast: \t" + str(round(commits_year_forecast)))
+print("\n")
+
